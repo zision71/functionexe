df3742f7fbd579aab290c510de2ffd687698bdff
zision71 <61615097+zision71@users.noreply.github.com>
2020-04-20 15:06:06 +0100

    Add files via upload

---
+++ week1.js
@@ -0,0 +1,13 @@
+// function that returns the year someone is born given their age as input
+
+function year(age) {
+  return 2020 - age;
+}
+console.log(year(25));
+
+// function that takes someone's name and age as input and returns a string that states the person's name and year they were born in a sentence
+
+function nameage(name, age) {
+  return "My Name is" + " " + name + " " + "My Birth Year is:" + year(age);
+}
+console.log(nameage("jhon", 25));
